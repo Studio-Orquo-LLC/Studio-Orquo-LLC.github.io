d85559404c65f971b3e6f35dc184e97ad844d6cd
AnakinTrotter <anakin.trotter@gmail.com>
2021-07-03 20:32:17 -0700

    Added dropdown menu
    Mobile and small devices can now change pages by accessing the dropdown menu added using react.

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,1 +1,25 @@
-// todo
+class Menu extends React.Component {
+    static toggled = false;
+
+    render() {
+        if(!Menu.toggled)
+            return(
+                <>
+                </>
+            )
+        return (
+            <>
+                <div id="dropdown">
+                    <a href="index.html" class="dropoption">Home</a>
+                    <a href="about.html" class="dropoption">About</a>
+                    <a href="devlogs.html" class="dropoption">Devlogs</a>
+                </div>
+            </>
+        )
+    }
+}
+
+function toggle() {
+    Menu.toggled = !Menu.toggled;
+    ReactDOM.render(<Menu />, document.querySelector("#react-dropdown"));
+}
