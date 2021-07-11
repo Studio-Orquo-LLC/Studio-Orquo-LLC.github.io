7e2b314990b120bd0d2e7c2b63e642c9e06dbf60
Anakin Trotter <anakin.trotter@gmail.com>
2021-07-11 13:04:57 -0700

    Sort of working swipe

--- scripts/splash.js
+++ scripts/splash.js
@@ -1,6 +1,6 @@
 class Splash extends React.Component {
     static idx = 0;
-    static delay = 5000;
+    static delay = 7000;
     getId = () => {
         const id = shortid.generate();
         console.log(id);
@@ -12,20 +12,29 @@ class Splash extends React.Component {
         } else if (Splash.idx > 2) {
             Splash.idx = 0;
         }
+        var p1 = document.getElementById("progress-1")
+        var p2 = document.getElementById("progress-2")
+        var p3 = document.getElementById("progress-3")
+        p1.style.backgroundColor = "white"
+        p2.style.backgroundColor = "white"
+        p3.style.backgroundColor = "white"
         switch (Splash.idx) {
             case 0:
+                p1.style.backgroundColor = "CornflowerBlue"
                 return (
                     
                         <img src="images/testimage.jpg" id="bg" class="splash" key={this.getId()}></img>
                     
                 )
             case 1:
+                p2.style.backgroundColor = "CornflowerBlue"
                 return (
                     <>
                         <img src="images/testimage2.jpg" id="bg" class="splash" key={this.getId()}></img>
                     </>
                 )
             case 2:
+                p3.style.backgroundColor = "CornflowerBlue"
                 return (
                     <>
                         <img src="images/testimage3.jpg" id="bg" class="splash" key={this.getId()}></img>
@@ -37,16 +46,8 @@ class Splash extends React.Component {
 
 ReactDOM.render(<Splash />, document.querySelector("#react-splash"));
 var tid = window.setTimeout(right, Splash.delay)
-var pid = window.setTimeout(progress, Splash.delay / 120)
-
-function progress() {
-    ProgressBar.setValue(ProgressBar.value + 1)
-    window.clearInterval(pid)
-    this.pid = window.setTimeout(progress, Splash.delay / 120)
-}
 
 function left() {
-    ProgressBar.setValue(0)
     Splash.idx--;
     ReactDOM.render(<Splash />, document.querySelector("#react-splash"))
     window.clearInterval(tid)
@@ -54,9 +55,55 @@ function left() {
 }
 
 function right() {
-    ProgressBar.setValue(0)
     Splash.idx++;
     ReactDOM.render(<Splash />, document.querySelector("#react-splash"))
     window.clearInterval(tid);
     this.tid = window.setTimeout(right, Splash.delay)
+}
+
+let pageWidth = window.innerWidth || document.body.clientWidth;
+let treshold = Math.max(1,Math.floor(0.01 * (pageWidth)));
+let touchstartX = 0;
+let touchstartY = 0;
+let touchendX = 0;
+let touchendY = 0;
+
+const limit = Math.tan(45 * 1.5 / 180 * Math.PI);
+const gestureZone = document.getElementById('pictures');
+
+gestureZone.addEventListener('touchstart', function(event) {
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
+}, false);
+
+gestureZone.addEventListener('touchend', function(event) {
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture(event);
+}, false);
+
+function handleGesture(e) {
+    let x = touchendX - touchstartX;
+    let y = touchendY - touchstartY;
+    let xy = Math.abs(x / y);
+    let yx = Math.abs(y / x);
+    if (Math.abs(x) > treshold || Math.abs(y) > treshold) {
+        if (yx <= limit) {
+            if (x < 0) {
+                right()
+            } else {
+                left()
+            }
+        }
+        // if (xy <= limit) {
+        //     if (y < 0) {
+        //         console.log("top");
+        //     } else {
+        //         console.log("bottom");
+        //     }
+        // }
+    } 
+    // else {
+    //     console.log("tap");
+    // }
 }
